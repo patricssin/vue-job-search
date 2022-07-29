9e76ee2c858cd09e8b7d620c28971aec82f8e4d3
patricssin <bianbianmomo@126.com>
2022-07-29 10:49:45 +0800

    search form

--- tests/unit/components/ActionButton.test.js
+++ tests/unit/components/ActionButton.test.js
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 
-import ActionButton from "@/components/ActionButton";
+import ActionButton from "@/components/Shared/ActionButton";
 
 describe("ActionButton", () => {
   it("render text", () => {
